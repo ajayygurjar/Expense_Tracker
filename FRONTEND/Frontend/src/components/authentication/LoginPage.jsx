ea8768986565cd0810b204a483926ab8414f8d54
ajayygurjar <ajay1651@gmail.com>
2025-12-29 01:46:03 +0530

    backend api of the login

--- FRONTEND/Frontend/src/components/authentication/LoginPage.jsx
+++ FRONTEND/Frontend/src/components/authentication/LoginPage.jsx
@@ -1,15 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
+
 const LoginPage=()=>{
+    const [loginData,setLoginData]=useState({
+        email:'',
+        password:''
+    })
+
+    const changeHandler=(e)=>{
+        const {name,value}=e.target;
+
+        setLoginData((prev)=>({
+            ...prev,[name]:value
+        }))
+    }
+
+    const handleLogin=async(e)=>{
+        e.preventDefault();
+        console.log(loginData);
+        try {
+            
+            const res=await axios.post(`http://localhost:5000/api/login`,loginData);
+            alert(res.data.message);
+            setLoginData({email:'',password:''});
+        } catch (error) {
+            console.error(error)
+            alert(error.response.data.message)
+        }
+    }
 
     return (<>
     <h1>Login Page</h1>
 
-    <form>
+    <form onSubmit={handleLogin}>
 
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email"  required/>
+        <input type="email" id="email"  name="email" value={loginData.email} onChange={changeHandler} required/>
         
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password"  required/>
+        <input type="password" id="password"  name="password" value={loginData.password} onChange={changeHandler} required/>
         <button>Submit</button>
     </form>
     
